f3d664458c73aebe050826fd9d7ccca99c199e70
rodsouto <rodrigo_7@live.com.ar>
2023-11-04 02:13:51 -0300

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,29 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   define: {
-    global: 'globalThis',
+    global: (() => {
+      if (command !== 'build') return 'globalThis'
+      let globalVariable = 'globalThis'
+      try {
+        // Try to import @safe-global/safe-apps-provider
+        require.resolve('@safe-global/safe-apps-provider')
+        // Try to import @safe-global/safe-apps-sdk
+        require.resolve('@safe-global/safe-apps-sdk')
+        // If both modules are found, return the custom global variable
+        globalVariable = 'global'
+      } catch (e) {
+        // If either module is not found, fallback to globalThis
+        globalVariable = 'globalThis'
+      }
+      return globalVariable
+    })(),
+  },
+  build: {
+    rollupOptions: {
+      external: ['@safe-globalThis/safe-apps-provider', '@safe-globalThis/safe-apps-sdk'],
+    },
   },
   resolve: {
     alias: {
@@ -13,4 +33,4 @@ export default defineConfig({
     },
   },
   plugins: [react()],
-})
+}))
